fa871a32c24896b9158ae9c8f4de203f951c19de
bro-alex <br.terehov@gmail.com>
2021-11-27 04:31:31 -0400

    psitest old3

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('/strtest/index');
 });
 
+Route::get('/test', function () {
+    return view('/strtest/subtest/index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
