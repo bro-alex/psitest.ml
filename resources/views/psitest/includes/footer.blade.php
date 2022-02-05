bfbec636b05f4c92e01bf4bfe0cc61dd9b5aa998
bro-alex <br.terehov@gmail.com>
2022-02-05 08:43:09 -0400

    adap1

--- resources/views/psitest/includes/footer.blade.php
+++ resources/views/psitest/includes/footer.blade.php
@@ -5,7 +5,7 @@
 </div>
 <div class="footer_top_container_text">
     <div class="fotter_top_contact">@korneichuk_vn</div>
-    <div class="footer_top_text">Автор трансформационных практик и собственной методики трансформации психотравм. <div class="footer_top_text_1">&ensp;Подробнее об этом в моём авторском курсе "Погружение"</div></div>
+    <div class="footer_top_text">Автор трансформационных практик и собственной методики трансформации психотравм. <div class="footer_top_text_1">&ensp;Подробнее об этом на моём сайте</div></div>
 </div>
 </div>
 <div class="footer_bottom">
@@ -25,6 +25,28 @@
 </div>
         </div>
     </div>
+
+
+    <div class="footer_bottom_container3">
+    <div class="footer_bottom_container_zag1">Курсы</div>
+<!--        <div class="footer_bottom_container_zag2">Контакты:</div>
+        <div class="footer_bottom_container_contacts">
+        <div class="footer_bottom_container_contacts_telega">
+        <div class="footer_bottom_container_contacts_icon"><img src="/img/telega.png" alt="Телеграмм"></div>
+        <div class="footer_bottom_container_contacts_contact">AkademiaVK</div>
+</div>
+
+<div class="footer_bottom_container_contacts_email">
+        <div class="footer_bottom_container_contacts_icon"><img src="/img/email.png" alt="Почта"></div>
+        <div class="footer_bottom_container_contacts_contact">v.korneichuk@bk.ru</div>
+</div>
+<div class="footer_bottom_container_contacts_skype">
+        <div class="footer_bottom_container_contacts_icon"><img src="/img/skype.png" alt="Skype"></div>
+        <div class="footer_bottom_container_contacts_contact">VKconsultation</div>
+</div>
+    </div>--></div>
+
+
     <div class="footer_bottom_container2">
     <div class="footer_bottom_container_zag1">Связаться с нами</div>
         <div class="footer_bottom_container_zag2">Контакты:</div>
@@ -43,6 +65,8 @@
         <div class="footer_bottom_container_contacts_contact">VKconsultation</div>
 </div>
     </div></div>
+
+
     <div class="footer_bottom_container_soc">
     <div class="footer_bottom_container_zag1">Мы в соц. сетях:</div>
     <div class="footer_bottom_container_containerSoc">
@@ -55,5 +79,16 @@
     </div></div>
     </div>
     <div class="fotter_bottom_copyright">© Meditation, 2021. All rights reserved</div>
+    <div class="fotter_bottom_copyright">|| Договор-оферта на оказание информационно-консультационных услуг ||
+
+|| Политика конфиденциальности персональных данных ||
+
+|| Условия оплаты и политика возврата оплаты банковскими картами для CloudPayments ||
+
+|| Условия оплаты и политика возврата оплаты банковскими картами ||
+
+|| Согласие на использование фото и видеоматериалов ||
+
+|| Отказ от ответственности ||</div>
 </div>
     </div>
